7116e37ced0ca38b4b06815aabef0bc3805d497a
Mattia Russo <russo.1918968@studenti.uniroma1.it>
2022-04-25 14:36:15 +0200

    Aggiunto un po' di stile alla nav del login; da completare

--- php/SignInUp/login.php
+++ php/SignInUp/login.php
@@ -15,8 +15,8 @@
         <header>
             <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="index.html">
-                        <img src="../pic/logo/10_crypto/logo.png" alt="" width="100" height="40">
+                    <a class="navbar-brand" href="../../html/index.html">
+                        <img src="../../pic/logo/10_crypto/small_logo.png" width="20" height="20">
                     </a>
                   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                    <!--<span class="navbar-toggler-icon"></span>-->   
@@ -25,7 +25,7 @@
                   <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                       <li class="nav-item">
-                        <a class="nav-link " aria-current="page" href="#">Home</a>
+                        <a class="nav-link " aria-current="page" href="../../html/index.html">Home</a>
                       </li>
                       <li class="nav-item">
                         <a class="nav-link" href="#">About</a>
