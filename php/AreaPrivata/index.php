dd94fdc364362eca8caf7318470dbf23297c2cae
Mattia Russo <russo.1918968@studenti.uniroma1.it>
2022-04-30 22:27:45 +0200

    Aggiunto il link alla pagina per creare schede

--- php/AreaPrivata/index.php
+++ php/AreaPrivata/index.php
@@ -8,7 +8,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-    <meta charset="UTF-8">
+        <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="../../css/private-style.css" rel="stylesheet">
@@ -53,11 +53,13 @@
 
             <!-- Bottone per aggiungere una nuova card-->
             <div class="card new-card">
-                <button class="new-card-button">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="new-card-pic" fill="currentColor" class="bi bi-file-plus-fill" viewBox="0 0 16 16">
-                        <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM8.5 6v1.5H10a.5.5 0 0 1 0 1H8.5V10a.5.5 0 0 1-1 0V8.5H6a.5.5 0 0 1 0-1h1.5V6a.5.5 0 0 1 1 0z"/>
-                    </svg>
-                </button>
+                <a href="NewForm/new.php">
+                    <button class="new-card-button">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="new-card-pic" fill="currentColor" class="bi bi-file-plus-fill" viewBox="0 0 16 16">
+                            <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM8.5 6v1.5H10a.5.5 0 0 1 0 1H8.5V10a.5.5 0 0 1-1 0V8.5H6a.5.5 0 0 1 0-1h1.5V6a.5.5 0 0 1 1 0z"/>
+                        </svg>
+                    </button>
+                </a>
             </div>
 
             <!--Dove andranno le schede scaricate dal database-->
