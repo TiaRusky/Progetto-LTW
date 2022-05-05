8d1dc1221135fad9022e18d1fe3e17f33e5ae34d
Claudio Schiavella <schiavella.1884561@studenti.uniroma1.it>
2022-05-05 17:37:21 +0200

    Gestione contattaci

--- js/scroll.js
+++ js/scroll.js
@@ -1,14 +1,13 @@
 //Non uso CSS, Jquery è cross platform
 //Con il menù hamburger, non si ottiene la fascia piena
 $(document).ready(function() {
-    $('.navbar a').on('click', function(ev) {
+    $('.navbar a').on('click', function() {
         var href = $(this).attr('href');
         var nav_h = $('.navbar').height() + 10;
 
         $('html, body').animate({
             'scrollTop': $(href).offset().top - nav_h
         }, 200);
-
     });
 });
 
--- js/sendMail.js
+++ js/sendMail.js
@@ -1,1 +1,15 @@
-window.alert("Mani in pasta, sorry")
+function sendEmail() {
+    event.preventDefault();
+
+    var mail = document.contactus.inputEmail.value;
+    var obj = document.contactus.inputObject.value;
+    var msg = document.contactus.inputMsg.value;
+
+    window.open('mailto:c.schiavella@gmail.com?from=mail&subject=obj&body=msg');
+
+    window.alert('Mail inviata!');
+
+    document.contactus.inputEmail.value = '';
+    document.contactus.inputObject.value = '';
+    document.contactus.inputMsg.value = '';
+}
