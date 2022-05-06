15a592ac4a8658d280e902ed70d621a3c03b00b1
Claudio Schiavella <schiavella.1884561@studenti.uniroma1.it>
2022-05-06 17:26:41 +0200

    Disaccopiamento stile navbar

--- php/SignInUp/login.php
+++ php/SignInUp/login.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="../../css/nav-style.css" rel="stylesheet">
         <link href="../../css/login-style.css" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
@@ -12,35 +13,40 @@
         <title>TechFit - Login</title>
     </head>
     <body>
-        <header>
-            <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="../../html/index.html">
-                        <img src="../../pic/logo/10_crypto/small_logo.png" class="home-logo">
-                    </a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                    </button>
-                  <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                      <li class="nav-item">
-                        <a class="nav-link " aria-current="page" href="../../html/index.html">Home</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#">About</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#">Team</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link">Gallery</a>
-                      </li>
+    <header class="sticky-top">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#home">
+                    <img src="../../pic/logo/10_crypto/small_logo.png" class="home-logo">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="mynavbar">
+                    <ul class="navbar-nav me-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="../../html/index.html#about">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../../html/index.html#team">Team</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../../html/index.html#gallery">Galleria</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../../html/index.html#products">Prodotti</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../../html/index.html#contacts">Contattaci</a>
+                        </li>
                     </ul>
-    
-                  </div>
+                    <form class="d-flex">
+                        <a href="../php/AreaPrivata/index.php" class="login-button" type="button">Login</a>
+                    </form>
                 </div>
-              </nav>
-        </header>
+            </div>
+        </nav>
+    </header>
 
         <!--Form di login e signin-->
         <div class="container">
