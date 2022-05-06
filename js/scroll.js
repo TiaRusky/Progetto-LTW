6fcc3f38ae1fb0086e8d6b47ce5e75ff8ad67d62
Claudio Schiavella <schiavella.1884561@studenti.uniroma1.it>
2022-05-06 14:45:37 +0200

    Risolto problema pulsante home

--- js/scroll.js
+++ js/scroll.js
@@ -1,23 +1,29 @@
 //Non uso CSS, Jquery è cross platform
 //Con il menù hamburger, non si ottiene la fascia piena
 $(document).ready(function() {
+    let sameValue = 1500;
+    var nav_h = $('.navbar').height() + 10;
+
     $('.navbar a').on('click', function() {
         var href = $(this).attr('href');
-        var nav_h = $('.navbar').height() + 10;
+
 
         $('html, body').animate({
             'scrollTop': $(href).offset().top - nav_h
         }, 200);
     });
-});
 
-$(document).ready(function() {
-    $('.home-button').on('click', function(ev) {
-        var nav_h = $('.navbar').height() + 10;
-        console.log("in");
 
+
+    $(".home-button").on('click', function() {
         $('html, body').animate({
-            'scrollTop': $('#products').offset().top - nav_h
-        }, 300);
+            'scrollTop': $('footer').offset().top - nav_h
+        }, 700, 'swing');
+        //then come back
+        setTimeout(() => {
+            $('html, body').animate({
+                'scrollTop': $('#about').offset().top - nav_h
+            }, sameValue, 'swing');
+        }, sameValue);
     });
 });
