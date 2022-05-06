4d36b0240bdc3f624271cf3e966635e28dccdad0
Claudio Schiavella <schiavella.1884561@studenti.uniroma1.it>
2022-05-06 17:11:11 +0200

    Risolto bug mail contattataci

--- js/sendMail.js
+++ js/sendMail.js
@@ -1,5 +1,5 @@
 function sendEmail() {
-    event.preventDefault();
+    //    event.preventDefault();
 
     var mail = document.contactus.inputEmail.value;
     var obj = document.contactus.inputObject.value;
@@ -9,7 +9,7 @@ function sendEmail() {
     console.log(obj);
     console.log(msg);
 
-    window.open('mailto:c.schiavella@gmail.com?from=' + mail + '&subject=' + obj + '&body=' + msg);
+    window.open('mailto:techfit.web@gmail.com?to=' + mail + '&subject=' + obj + '&body=' + msg);
 
     window.alert('Mail inviata!');
 
