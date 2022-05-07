dfd4ea5f240b93f1ee0c254decaa3cd11a50fc4c
Claudio Schiavella <schiavella.1884561@studenti.uniroma1.it>
2022-05-07 18:24:40 +0200

    Sistemato nav area privata

--- php/AreaPrivata/index.php
+++ php/AreaPrivata/index.php
@@ -12,6 +12,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="../../css/private-style.css" rel="stylesheet">
+        <link href="../../css/nav-style.css" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
@@ -38,7 +39,7 @@
                         <!-- Barra di ricerca per le schede -->
                         <form class="d-flex">
                             <input class="form-control me-2" type="search" placeholder="Cerca scheda" aria-label="Search">
-                            <button class="btn btn-outline-success" type="submit">Cerca</button>
+                            <input class="btn-src" type="submit" value="Cerca" name="searchSubmit">
                         </form>
 
                         <!-- Bottone di logout -->
