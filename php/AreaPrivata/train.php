eb828cfeeaf4737e78c5ad254cbd6989216a1eab
Claudio Schiavella <schiavella.1884561@studenti.uniroma1.it>
2022-05-13 19:29:11 +0200

    Modifica bozza train

--- php/AreaPrivata/train.php
+++ php/AreaPrivata/train.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="../pic/logo/10_crypto/small_logo.ico" type="image/icon type">
+    <link rel="icon" href="../../pic/logo/10_crypto/small_logo.ico" type="image/icon type">
     <link href="../../css/train-style.css" rel="stylesheet" type="text/css">
     <link href="../../css/nav-style.css" rel="stylesheet" type="text/css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -41,12 +41,25 @@
         </nav>
     </header>
 
+    <!-- Sicuramente ogni pallina del progress delle serie dovrà essere gestita con un echo, le scrivo comunque statiche come modello -->
+
     <div class="container-fluid">
         <div class="player">
             <h1>Nome esercizio</h1>
             <h2>- Modalità di esecuzione - </h2>
             <div class="timers">
-                <img src="../../pic/caution-sign.gif" style="width:150px;height:150px;">    
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            <ul id="progress-bar" class="progressbar">
+                                <li class="active">Details</li>
+                                <li>Address</li>
+                                <li>add friends</li>
+                                <li>Confirm</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
                 <div class="circle-wrap">
                     <div class="circle">
                         <div class="mask half">
@@ -57,7 +70,7 @@
                         </div>
                         <div class="inside-circle"> 75% </div>
                     </div>
-                </div>
+                </div>   
             </div>
             <div class="buttons">
                 <a href="#" class="btn btn-primary btn-lg">Recupero</a>
