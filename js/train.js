7ba9936f2ca648c6cf54ca5d0971a91cd6f99021
Mattia Russo <russo.1918968@studenti.uniroma1.it>
2022-05-14 16:03:51 +0200

    Aggiunto il timer per il recupero

--- js/train.js
+++ js/train.js
@@ -1,3 +1,28 @@
 $(document).ready(function(){
+    /*
+    $("#countdown").countdown360({
+        radius      : 60.5,
+        seconds     : 120,
+        strokeWidth : 15,
+        fillStyle   : '#222222',
+        strokeStyle : '#eedf69',
+        fontSize    : 50,
+        fontColor   : '#eedf69',
+        autostart: false,
+        onComplete  : function () { console.log('completed') }
+      }).start();
+    */
+   var $countdonw = $("#countdown").countdown360({
+        radius      : 60.5,
+        seconds     : 0,
+        strokeWidth : 15,
+        fillStyle   : '#222222',
+        strokeStyle : '#eedf69',
+        fontSize    : 50,
+        fontColor   : '#eedf69',
+        autostart   : true,
+        onComplete  : function () { console.log('completed') }
+    });
+
     
 });
