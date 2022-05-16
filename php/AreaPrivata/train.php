82bc8749586fdb3e69990b821ce248bde9327ff7
Claudio Schiavella <schiavella.1884561@studenti.uniroma1.it>
2022-05-16 12:39:37 +0200

    Modal train

--- php/AreaPrivata/train.php
+++ php/AreaPrivata/train.php
@@ -18,7 +18,7 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <script src="../../js/train.js"></script>
-        <script src="../../js/redo.js"></script>
+        <script src="../../js/index.js"></script>
         <script src="../../js/jquery.countdown360.js"></script>
         <title>TechFit - Train di <?php echo $_SESSION['username'];?></title>
     </head>
@@ -63,13 +63,69 @@
                 <div class="end-train">                
                     <div class="end-msg">Complimenti!<br> Hai terminato l'allenamento!</div>
                     <a href="./index.php" class="train-btn">Home</a>
-                    <a href="./index.php" class="train-btn">Continua allenamento</a>
+                    <a class="train-btn" data-bs-toggle="modal" data-bs-target="#modalTrain">Continua allenamento</a>
                     <div class="firework"></div>
                     <div class="firework"></div>
                     <div class="firework"></div>
                 </div>
             </div>
         </div> 
+        <!-- Modal per scegliere il gruppo muscolare da allenare-->
+        <div class="modal fade" id="modalTrain" tabindex="-1" aria-labelledby="modalTrainLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="modalTrainLabel">Nome scheda</h5>
+                        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <h3>Scegli quale gruppo allenare</h3>
+                        <div class="grid-gruppi">
+                            <button class="group-btn" id="btn-dorsali">
+                                <img src="../../pic/new/icons8-bodybuilder-96-text.png" alt="logo-dorsali">
+                            </button>
+
+                            <button class="group-btn" id="btn-tricipiti">
+                                <img src="../../pic/new/icons8-triceps-96-text.png" alt="logo-tricipiti">
+                            </button>
+
+                            <button class="group-btn" id="btn-petto">
+                                <img src="../../pic/new/icons8-chest-96-text.png" alt="logo-petto">
+                            </button> 
+
+                            <button class="group-btn" id="btn-bicipiti">
+                                <img src="../../pic/new/icons8-biceps-96-text.png" alt="logo-bicipiti">
+                            </button>
+
+                            <button class="group-btn" id="btn-gambe">
+                                <img src="../../pic/new/icons8-quadriceps-96-text.png" alt="logo-gambe">
+                            </button>
+
+                            <button class="group-btn" id="btn-spalle">
+                                <img src="../../pic/new/icons8-shoulders-96-text.png" alt="logo-spalle">
+                            </button>
+                        </div>
+                        
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <!--Alert-->
+        <?php
+            $fullUrl = "http://".$_SERVER['HTTP_HOST']."".$_SERVER["REQUEST_URI"];
+            //Gestione fallimenti registrazione
+            if(strpos($fullUrl,"err=noExc") == true){   //La scheda selezionata per l'allenamento,non ha esercizi per quel gruppo
+                echo "<div class='error'>
+                        <span class='closebtn'>&times;</span>
+                        <span class='closebtn-fill'>&times;</span>
+                        <p  class='err-msg'><i class='fas fa-exclamation-triangle'></i> Non ci sono esercizi per quel gruppo
+                        </p></div>";
+            }
+        ?>
+
+
     </body>
 
     <svg xmlns="http://www.w3.org/2000/svg">
